6c4eb058b1439c86f72dd4e8c5e2c4403f799870
modest-Hamilton <864090381@qq.com>
2023-05-05 17:22:45 +0800

    add DKG

---
+++ src/main/java/KeyGrouping/DKGrouping/DKGStorm.java
@@ -0,0 +1,133 @@
+package KeyGrouping.DKGrouping;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import KeyGrouping.DKGrouping.builder.DKGBuilder;
+import KeyGrouping.DKGrouping.builder.DKGHash;
+import org.apache.storm.generated.GlobalStreamId;
+import org.apache.storm.grouping.CustomStreamGrouping;
+import org.apache.storm.task.WorkerTopologyContext;
+
+
+/**
+ * @author Nicolo Rivetti
+ * 
+ * 
+ */
+public class DKGStorm implements CustomStreamGrouping, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8611934642809524449L;
+
+	public final double theta;
+	public final double factor;
+	public final double epsilonFactor;
+
+	private List<Integer> targetTasks;
+
+	public final int learningLenght;
+	private int m = 0;
+	private DKGBuilder builder;
+	private DKGHash hash;
+	private IKey key;
+
+	/**
+	 * 
+	 * Implements the CustomStreamGrouping interface offered by the Apache Storm
+	 * API This implementation leverages the Distribution-Aware Key Grouping
+	 * (DKG) algorithm
+	 * 
+	 * @param theta
+	 *            double value in (0,1], heavy hitter threshold, i.e., all keys
+	 *            with an empirical probability larger than or equal to theta
+	 *            belong to the heavy hitter set.
+	 * @param factor
+	 *            double value >=1, set the number of buckets of sparse items to
+	 *            factor * k (number of available instances).
+	 * @param learningLenght
+	 *            number of tuples that will be used to learn the key value
+	 *            distribution (these tuples are discarded).
+	 * @param key
+	 *            An instance of an implementation of the IKey interface,
+	 *            returning an integer value representing the key, through which
+	 *            the operator state is partitioned, associated with each tuple
+	 *            of the stream.
+	 * @param epsilonFactor
+	 *            defines the ration between the Space Saving's Heavy Hitter
+	 *            threshold theta and precision parameter epsilon: epsilon =
+	 *            theta / epsilonFactor.
+	 */
+	public DKGStorm(double theta, double factor, int learningLenght, IKey key, double epsilonFactor) {
+		super();
+		this.theta = theta;
+		this.factor = factor;
+		this.epsilonFactor = epsilonFactor;
+		this.learningLenght = learningLenght;
+		this.key = key;
+	}
+
+	/**
+	 * 
+	 * Implements the CustomStreamGrouping interface offered by the Apache Storm
+	 * API This implementation leverages the Distribution-Aware Key Grouping
+	 * (DKG) algorithm
+	 * 
+	 * @param theta
+	 *            double value in (0,1], heavy hitter threshold, i.e., all keys
+	 *            with an empirical probability larger than or equal to theta
+	 *            belong to the heavy hitter set.
+	 * @param factor
+	 *            double value >=1, set the number of buckets of sparse items to
+	 *            factor * k (number of available instances).
+	 * @param learningLenght
+	 *            number of tuples that will be used to learn the key value
+	 *            distribution (these tuples are discarded).
+	 * @param key
+	 *            An instance of an implementation of the IKey interface,
+	 *            returning an integer value representing the key, through which
+	 *            the operator state is partitioned, associated with each tuple
+	 *            of the stream.
+	 */
+	public DKGStorm(double theta, double factor, int learningLenght, IKey key) {
+		super();
+		this.theta = theta;
+		this.factor = factor;
+		this.epsilonFactor = 2.0;
+		this.learningLenght = learningLenght;
+		this.key = key;
+	}
+
+	@Override
+	public void prepare(WorkerTopologyContext context, GlobalStreamId stream, List<Integer> targetTasks) {
+
+		this.targetTasks = targetTasks;
+
+		int k = targetTasks.size();
+
+		this.builder = new DKGBuilder(theta, epsilonFactor, k, factor);
+
+	}
+
+	@Override
+	public List<Integer> chooseTasks(int taskId, List<Object> values) {
+
+		List<Integer> ret = new ArrayList<Integer>(1);
+		if (m < learningLenght) {
+			m++;
+			this.builder.newSample(key.get(values));
+			if (m == learningLenght) {
+				this.hash = this.builder.build();
+			}
+		} else {
+			ret.add(this.targetTasks.get(this.hash.map(key.get(values))));
+		}
+
+		return ret;
+
+	}
+
+}
---
+++ src/main/java/KeyGrouping/DKGrouping/builder/DKGHash.java
@@ -0,0 +1,49 @@
+package KeyGrouping.DKGrouping.builder;
+
+import java.util.HashMap;
+
+/**
+ * @author Nicolo Rivetti
+ */
+public class DKGHash {
+
+	public final HashMap<Integer, Integer> mostFrequentMapping;
+	public final CWHashFunction hashfunction;
+	public final HashMap<Integer, Integer> hashFunctionMapping;
+
+	/**
+	 * 
+	 * This class encapsulates DKG's global mapping (Heavy Hitters to operator
+	 * instances and buckets of Sparse Items to instances)
+	 * 
+	 * @param mostFrequentMapping
+	 *            Heavy Hitters to operator instances mapping
+	 * @param hashfunction
+	 *            the hash function that partitions the Sparse Items into
+	 *            buckets.
+	 * @param hashFunctionMapping
+	 *            buckets of Sparse Items to instances mapping
+	 */
+	public DKGHash(HashMap<Integer, Integer> mostFrequentMapping, CWHashFunction hashfunction,
+			HashMap<Integer, Integer> hashFunctionMapping) {
+		super();
+		this.mostFrequentMapping = mostFrequentMapping;
+		this.hashfunction = hashfunction;
+		this.hashFunctionMapping = hashFunctionMapping;
+	}
+
+	/**
+	 * @param key
+	 *            a Heavy Hitter or Sparse Item key value.
+	 * @return the instances mapped to the provided Heavy Hitter or Sparse Item
+	 *         key.
+	 */
+	public int map(int key) {
+		Integer ret = mostFrequentMapping.get(key);
+		if (ret != null) {
+			return ret;
+		}
+
+		return hashFunctionMapping.get(hashfunction.hash(key));
+	}
+}
